df9f674c186b20c4cdc7073cdda83b3ed51f662c
Zhan JING <147552860+zhanquen@users.noreply.github.com>
2025-06-12 19:49:56 +0800

    Create Ch2.v

---
+++ Fondamentaux/Ch2.v
@@ -0,0 +1,13 @@
+From Coq Require Import Init.Prelude Unicode.Utf8.
+From mathcomp Require Import all_ssreflect.
+Set Implicit Arguments.
+Unset Strict Implicit.
+Unset Printing Implicit Defensive. 
+
+Section Théorie_des_ensembles.
+(**
+p30: P(x) is a proposition and P is called a predicate.
+*)
+Variable T : finType.
+
+End Théorie_des_ensembles.
