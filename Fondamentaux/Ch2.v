5b5c7cd94c55e759e9a59fbb4ec6984cec555c48
Zhan JING <147552860+zhanquen@users.noreply.github.com>
2025-06-14 14:05:45 +0800

    Update Ch2.v

--- Fondamentaux/Ch2.v
+++ Fondamentaux/Ch2.v
@@ -5,46 +5,48 @@ Unset Strict Implicit.
 Unset Printing Implicit Defensive. 
 
 Section Théorie_des_ensembles.
+
 (**
-p30: P(x) is a proposition and P is called a predicate.
+In this section, we use finType to create sets with finite elements, 
+however, we avoid using any properties related to the finity, 
+since the definition of sets in this chapter is more general.
 *)
-Check nat.
-Variables E F : finType.
-(**
-Hypothesis EM : forall A : Prop, (~ ~ A) = A.
-Lemma contra_inv : forall A B : Prop, (~ B -> ~ A) -> (A -> B).
+
+(* two results from the last chapter *)
+
+Lemma my_contra : forall A B : Prop, (A -> B) -> (~ B -> ~ A).
 Proof.
-move=> A B H.
-have H1 : ~ ~ A -> ~ ~ B by apply: impliesPn.
-by rewrite !EM in H1.
+by move=> A B H0 H1 H2; apply: H1; apply: H0.
 Qed.
-*)
-Proposition exneg_negforall (P : pred E):
+Hypothesis classic : forall A : Prop, (~ ~ A) = A.
+Lemma my_contra_inv : forall A B : Prop, (~ B -> ~ A) -> (A -> B).
+Proof.
+move=> A B H0.
+rewrite -(classic A) -(classic B).
+exact: my_contra.
+Qed.
+
+(* p30: P(x) is a proposition and P is called a predicate. *)
+Variables E F : finType.
+(* p43 Prop 2.2.2 no proof *)
+Proposition exneg_negforall (P : E -> Prop):
   (exists x : E, ~ P(x)) -> ~ (forall x : E, P(x)).
 Proof.
 case => x notPx H.
 by have Px := H x.
 Qed.
-Proposition negex_forallneg (P : pred E):
+Proposition negex_forallneg (P : E -> Prop):
   ~ (exists x : E, P(x)) -> forall x : E, ~ P(x).
 Proof.
 by move=> H x Px; case: H; exists x.
 Qed.
-
-(* p44 chinese translation error: no set F *)
-Proposition échange_pourtout (P : pred (E * F)) : 
-  (forall x : E, forall y : F, P(x,y)) <-> (forall y : F, forall x : E, P(x,y)).
+Proposition negforall_exneg (P : E -> Prop): 
+  ~ (forall x : E, P(x)) -> (exists x : E, ~ P(x)).
 Proof.
-by split=> [H y x|H x y]; apply: H.
+apply: my_contra_inv.
+rewrite classic=> H0.
+move=> x.
+rewrite -(classic (P x)).
+move : x.
+exact: negex_forallneg.
 Qed.
-
-Proposition échange_existe (P : pred (E * F)): 
-  (exists x : E, exists y : F, P(x,y)) <-> (exists y : F, exists x : E, P(x,y)).
-Proof.
-(* p44 Prop 2.2 no proof *)
-by split; [move=> [] x [] y Pxy|move=> [] y [] x Pyx]; 
-  (* the existence in the presequent could be moved into hypothesis *)
-  [exists y; exists x| exists x; exists y].
-  (* the existence is applied *)
-Qed.
-End Théorie_des_ensembles.
