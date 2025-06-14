a5fb8e0be6fee57da3a332feddfaaf5f05123c04
Zhan JING <147552860+zhanquen@users.noreply.github.com>
2025-06-14 17:19:44 +0800

    Update Ch2.v

--- Fondamentaux/Ch2.v
+++ Fondamentaux/Ch2.v
@@ -4,14 +4,6 @@ Set Implicit Arguments.
 Unset Strict Implicit.
 Unset Printing Implicit Defensive. 
 
-Section Théorie_des_ensembles.
-
-(**
-In this section, we use finType to create sets with finite elements, 
-however, we avoid using any properties related to the finity, 
-since the definition of sets in this chapter is more general.
-*)
-
 (* two results from the last chapter *)
 Check contra. (* ∀ c b : bool, (c → b) → ~~ b → ~~ c*)
 Check contraLR. (* ∀ c b : bool, (~~ c → ~~ b) → b → c*)
@@ -28,13 +20,19 @@ rewrite -(classic A) -(classic B).
 exact: my_contra.
 Qed.
 
+Section Quantificateur.
+
+(**
+In this section, we use finType to create sets with finite elements, 
+however, we avoid using any properties related to the finity, 
+since the definition of sets in this chapter is more general.
+*)
+
 (* p30: P(x) is a proposition and P is called a predicate. *)
 
-Variables E F : finType.
-
 (* p43 Prop 2.2.2 no proof *)
-Proposition exneg_negforall (P : pred E) :
-  (exists x : E, ~~ P(x)) -> ~ (forall x : E, P(x)).
+Proposition exneg_negforall (T : finType) (P : pred T) :
+  (exists x : T, ~~ P(x)) -> ~ (forall x : T, P(x)).
 Proof.
 case=> x.
 move/negP => notPx.
@@ -43,8 +41,8 @@ have H1 := H x.
 by [].
 Qed.
 
-Proposition negex_forallneg (P : pred E) :
-  ~ (exists x : E, P(x)) -> forall x : E, ~~ P(x).
+Proposition negex_forallneg (T : finType) (P : pred T) :
+  ~ (exists x : T, P(x)) -> forall x : T, ~~ P(x).
 Proof.
 move=> H x; apply/negP.
 move=> H1.
@@ -52,8 +50,8 @@ case: H.
 by exists x.
 Qed.
 
-Proposition negforall_exneg (P : pred E) : 
-  ~ (forall x : E, P(x)) -> (exists x : E, ~~ P(x)).
+Proposition negforall_exneg (T : finType) (P : pred T) : 
+  ~ (forall x : T, P(x)) -> (exists x : T, ~~ P(x)).
 Proof.
 apply: my_contra_inv.
 rewrite classic=> H0.
@@ -65,8 +63,8 @@ move: x.
 exact: negex_forallneg.
 Qed.
 
-Proposition forallneg_negex (P : pred E) : 
-  (forall x : E, ~~ P(x)) -> ~ (exists x : E, P(x)).
+Proposition forallneg_negex (T : finType) (P : pred T) : 
+  (forall x : T, ~~ P(x)) -> ~ (exists x : T, P(x)).
 Proof.
 apply: my_contra_inv.
 rewrite classic.
@@ -76,6 +74,7 @@ exists x.
 exact/negPn.
 Qed.
 
+Variables E F : finType.
 (* p44 chinese translation error: no set F *)
 Proposition échange_pourtout (P : pred (E * F)) : 
   (forall x : E, forall y : F, P(x,y)) <-> (forall y : F, forall x : E, P(x,y)).
@@ -93,16 +92,18 @@ by split; [move=> [] x [] y Pxy|move=> [] y [] x Pyx];
   (* the existence is applied *)
 Qed.
 
-Variable T : finType.
+End Quantificateur.
 
-Proposition inclusion_réflexitivité (A : {set T}) : A \subset A.
+Section Inclusion_de_deux_ensembles.
+
+Proposition inclusion_réflexitivité (E : finType) (A : {set E}) : A \subset A.
 Proof.
 apply/subsetP; rewrite /sub_mem.
 move=> x.
 by [].
 Qed.
 
-Proposition inclusion_transitivité (A B C : {set T}) :
+Proposition inclusion_transitivité (E : finType) (A B C : {set E}) :
   (A \subset B) && (B \subset C) -> (A \subset C).
 Proof.
 move/andP => [].
@@ -115,10 +116,10 @@ apply: H1.
 by [].
 Qed.
 
-Proposition double_inclusion_E (A B : {set T}) : 
-  (A \subset B) && (B \subset A) <-> (A == B).
+Proposition double_inclusion_E (E : finType) (A B : {set E}) : 
+  (A \subset B) && (B \subset A) = (A == B).
 Proof.
-split.
+apply/idP/idP.
 - move/andP. case=> AB BA. 
   move/subsetP in AB; rewrite /sub_mem in AB; move/subsetP in BA; rewrite /sub_mem in BA.
   apply/eqP; apply/setP; rewrite /eq_mem.
@@ -132,4 +133,40 @@ split.
   by [].
 Qed.
   
-End Théorie_des_ensembles.
+Proposition pourtout_vide_P (P : pred void) : forall x : void, P x.
+Proof.
+move=> x.
+have H : x \in set0 by [].
+move: H.
+rewrite in_set0.
+by [].
+Qed.
+
+Proposition existe_vide_P (P : pred void) : ~ (exists x : void, P x).
+Proof.
+apply: forallneg_negex.
+exact: pourtout_vide_P.
+Qed.
+
+Proposition partie_vide_ensemble (E : finType) (A : {set E}) :
+  set0 \subset A.
+Proof.
+apply/subsetP; rewrite /sub_mem.
+move=> x.
+by rewrite in_set0.
+Qed.
+
+Proposition powerset_vide_NE (E : finType) (A : {set E}) : 
+  ~~ ((powerset A) == set0).
+Proof.
+rewrite -subset0.
+apply/subsetP; rewrite /sub_mem.
+move=> H.
+have H1 := H A.
+rewrite powersetE inclusion_réflexitivité in_set0 in H1.
+move/implyP in H1.
+move: H1.
+by [].
+Qed.
+
+End Inclusion_de_deux_ensembles.
