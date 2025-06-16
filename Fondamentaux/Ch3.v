2ecdba67eda0ee84d684dd8807fe15a345dce31f
Zhan JING <147552860+zhanquen@users.noreply.github.com>
2025-06-16 21:56:29 +0800

    Update Ch3.v

--- Fondamentaux/Ch3.v
+++ Fondamentaux/Ch3.v
@@ -14,7 +14,24 @@ Proposition fonc_comp_assoc :
   h \o (g \o f) =1 (h \o g) \o f.
 Proof.
 rewrite/eqfun => x.
-by []. (* trivial by calculation *)
+rewrite /=. (* trivial by calculation *)
+by [].
+Qed.
+
+Proposition Neutralité_de_l'identité_gauche : 
+  (@ id F) \o f =1 f.
+Proof.
+rewrite/eqfun => x.
+rewrite /=.
+by [].
+Qed.
+
+Proposition Neutralité_de_l'identité_droite : 
+   f \o (@ id E) =1 f.
+Proof.
+rewrite/eqfun => x.
+rewrite /=.
+by [].
 Qed.
 
 End Définitions_et_premiers_exemples.
