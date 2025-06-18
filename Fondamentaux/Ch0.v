cee52bb1ccc41fd6286db62013fe395b86707546
Zhan JING <147552860+zhanquen@users.noreply.github.com>
2025-06-18 15:19:27 +0800

    Create Ch0.v

---
+++ Fondamentaux/Ch0.v
@@ -0,0 +1,9 @@
+(**
+A formalization project for the book "Fondamentaux de mathématiques"
+Properties in this book concern:
+  - Basic proof techniques (Chapter 4);
+  - Logics (Chapter 1);
+  - The set theory (Chapter 2);
+  - Function (Chapter 3);
+See
+  *)
