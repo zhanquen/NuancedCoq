7ca8540d670fceea19a631e7e65361f44ecc156a
Zhan JING <147552860+zhanquen@users.noreply.github.com>
2025-06-18 15:41:59 +0800

    Update Ch0.v

--- Fondamentaux/Ch0.v
+++ Fondamentaux/Ch0.v
@@ -1,9 +1,22 @@
 (**
+---------------------------------------------------------------------------------
 A formalization project for the book "Fondamentaux de mathématiques"
 Properties in this book concern:
   - Basic proof techniques (Chapter 4);
   - Logics (Chapter 1);
   - The set theory (Chapter 2);
   - Function (Chapter 3);
-See
-  *)
+
+Author: Zhan JING
+
+Please refer to
+  https://speit.sjtu.edu.cn/fr/about/news/37594
+for more details.
+
+Date: July 2025
+
+Thanks
+  Pierre Jouvelot 
+for his insights in Rocq proof assistant and Mathematical-Components libraries.
+---------------------------------------------------------------------------------
+*)
