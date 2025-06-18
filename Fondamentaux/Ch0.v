dd88b6ae289b24742cbf3a669ccad225dee28644
Zhan JING <147552860+zhanquen@users.noreply.github.com>
2025-06-18 16:31:52 +0800

    Update Ch0.v

--- Fondamentaux/Ch0.v
+++ Fondamentaux/Ch0.v
@@ -1,3 +1,9 @@
+From Coq Require Import Init.Prelude Unicode.Utf8.
+From mathcomp Require Import all_ssreflect.
+Set Implicit Arguments.
+Unset Strict Implicit.
+Unset Printing Implicit Defensive. 
+
 (**
 ---------------------------------------------------------------------------------
 A formalization project for the book "Fondamentaux de mathématiques"
@@ -20,3 +26,50 @@ Thanks
 for his insights in Rocq proof assistant and Mathematical-Components libraries.
 ---------------------------------------------------------------------------------
 *)
+
+Section Récapitulatif_des_méthodes_déjÀ_vues.
+
+Variables P Q R: Prop.
+
+Proposition dém_conjonction1 : P /\ Q.
+split.
+Admitted.
+
+Proposition dém_conjonction2 : P /\ Q.
+split; last first.
+Admitted.
+
+Proposition dém_implication1 : P -> Q.
+move=> H.
+Admitted.
+
+Hypothesis classic : forall A : Prop, (~ ~ A) = A.
+
+Lemma my_contra : forall A B : Prop, (A -> B) -> (~ B -> ~ A).
+Proof.
+by move=> A B H0 H1 H2; apply: H1; apply: H0.
+Qed.
+
+Lemma my_contra_inv : forall A B : Prop, (~ B -> ~ A) -> (A -> B).
+Proof.
+move=> A B H0.
+rewrite -(classic A) -(classic B).
+exact: my_contra.
+Qed.
+
+Proposition dém_implication2 : P -> Q.
+apply: my_contra_inv.
+Admitted.
+
+Proposition dém_équivalence1 : P <-> Q.
+split.
+Admitted.
+
+Proposition dém_équivalence2 (P1 : Prop) (H : P <-> P1) : P <-> Q.
+rewrite H.
+Admitted.
+
+Proposition distinction_cas : P \/ Q -> R.
+move => [].
+
+End Récapitulatif_des_méthodes_déjÀ_vues.
