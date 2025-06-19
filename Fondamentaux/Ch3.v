3423f8303c4cafff502e2deafd5b4c5b94b8e787
Zhan JING <147552860+zhanquen@users.noreply.github.com>
2025-06-19 22:38:53 +0800

    Update Ch3.v

--- Fondamentaux/Ch3.v
+++ Fondamentaux/Ch3.v
@@ -4,141 +4,124 @@ Set Implicit Arguments.
 Unset Strict Implicit.
 Unset Printing Implicit Defensive. 
 
-(**
----------------------------------------------------------------------------------
-A formalization project for the book "Fondamentaux de mathématiques"
-Properties in this book concern:
-  - Basic proof techniques (Chapter 4);
-  - Logics (Chapter 1);
-  - The set theory (Chapter 2);
-  - Function (Chapter 3);
+Section Définitions_et_premiers_exemples.
 
-Author: Zhan JING
+Variables (E F G: finType) (H : Type).
 
-Please refer to
-  https://speit.sjtu.edu.cn/fr/about/news/37594
-for more details.
+Variables (f : {ffun E → F}) (g : {ffun F → G}) (h : {ffun G → H}).
 
-Date: July 2025
-
-Thanks
-  Pierre Jouvelot 
-for his insights in Rocq proof assistant and Mathematical-Components libraries.
----------------------------------------------------------------------------------
-*)
-
-Section Récapitulatif_des_méthodes_déjÀ_vues.
-
-Variables P Q R: Prop.
-
-Proposition dém_conjonction1 : P /\ Q.
-split.
-Admitted.
-
-Proposition dém_conjonction2 : P /\ Q.
-split; last first.
-Admitted.
-
-Proposition dém_implication1 : P -> Q.
-move=> H.
-Admitted.
-
-Hypothesis classic : forall A : Prop, (~ ~ A) = A.
-
-Lemma my_contra : forall A B : Prop, (A -> B) -> (~ B -> ~ A).
+Proposition fonc_comp_assoc : 
+  h \o (g \o f) =1 (h \o g) \o f.
 Proof.
-by move=> A B H0 H1 H2; apply: H1; apply: H0.
-Qed.
-
-Lemma my_contra_inv : forall A B : Prop, (~ B -> ~ A) -> (A -> B).
-Proof.
-move=> A B H0.
-rewrite -(classic A) -(classic B).
-exact: my_contra.
-Qed.
-
-Proposition dém_implication2 : P -> Q.
-apply: my_contra_inv.
-Admitted.
-
-Proposition dém_équivalence1 : P <-> Q.
-split.
-Admitted.
-
-Proposition dém_équivalence2 (P1 : Prop) (H : P <-> P1) : P <-> Q.
-rewrite H.
-Admitted.
-
-Proposition distinction_cas1 : P \/ Q -> R.
-move => [].
-Admitted.
-
-Proposition distinction_cas2 : P \/ Q -> R.
-case.
-Admitted.
-
-Variable E : finType.
-
-Variable Pr : pred E.
-
-Proposition dém_prop_univ1 : forall x : E, Pr x.
-move=> x.
-Admitted.
-
-Proposition dém_prop_univ2 : (exists x0 : E, ~ Pr x0) -> ~ (forall x : E, Pr x).
-Proof.
-move => [] x0 H H0.
-have H1 := H0 x0.
+rewrite/eqfun => x.
+rewrite /=. (* trivial by calculation *)
 by [].
 Qed.
 
-(** 
-for the rest of the methods , refer to Ch2.v
-*)
-
-Proposition exneg_negforall (T : finType) (P : pred T) :
-  (exists x : T, ~~ P(x)) -> ~ (forall x : T, P(x)).
+Proposition Neutralité_de_l'identité_gauche : 
+  (@ id F) \o f =1 f.
 Proof.
-case=> x.
-move/negP => notPx.
-move=> H.
-have H1 := H x.
+rewrite/eqfun => x.
+rewrite /=.
 by [].
 Qed.
 
-Proposition negex_forallneg (T : finType) (P : pred T) :
-  ~ (exists x : T, P(x)) -> forall x : T, ~~ P(x).
+Proposition Neutralité_de_l'identité_droite : 
+   f \o (@ id E) =1 f.
 Proof.
-move=> H x; apply/negP.
-move=> H1.
-case: H.
-by exists x.
+rewrite/eqfun => x.
+rewrite /=.
+by [].
 Qed.
 
-Proposition negforall_exneg (T : finType) (P : pred T) : 
-  ~ (forall x : T, P(x)) -> (exists x : T, ~~ P(x)).
+(* p100 typo : h \o (g \o h) *)
+
+Proposition image_d'une_composée :
+  (g \o f) @: E \subset g @: F.
 Proof.
-apply: my_contra_inv.
-rewrite classic=> H0.
-move=> x.
-rewrite -(classic (P x)).
-move/negP=> H.
-move: H; apply/negP.
-move: x.
-exact: negex_forallneg.
+apply/subsetP; rewrite/sub_mem => z.
+move/imsetP. (** definition of imset *)
+move => [] x H0.
+rewrite /= => H1.
+apply/imsetP.
+exists (f x); last by [].
+by [].
 Qed.
 
-Proposition forallneg_negex (T : finType) (P : pred T) : 
-  (forall x : T, ~~ P(x)) -> ~ (exists x : T, P(x)).
+Proposition image_vide : 
+  f @: set0 = set0.
 Proof.
-apply: my_contra_inv.
-rewrite classic.
-move => [] x H.
-apply: exneg_negforall.
-exists x.
-exact/negPn.
+apply/eqP.
+rewrite -subset0.
+apply/subsetP; rewrite/sub_mem => y.
+move/imsetP.
+move=> [] x.
+rewrite in_set.
+by [].
 Qed.
-(* ??? unicit/ *)
 
-Search (exists! _, _).
-End Récapitulatif_des_méthodes_déjÀ_vues.
+Variables (E' E'' : {set E}).
+Hypothesis (E'E : E' \subset E) (E''E : E'' \subset E).
+Proposition image_partie :
+  E' \subset E'' -> f @: E' \subset f @: E''.
+Proof.
+move=> H0.
+apply/subsetP; rewrite/sub_mem => y.
+move/imsetP => [] x H1 H2.
+apply/imsetP.
+move/subsetP in H0.
+rewrite/sub_mem in H0.
+exists x; last by [].
+apply: H0.
+by [].
+Qed.
+
+Proposition image_union :
+  f @: (E' :|: E'') = f @: E' :|: f @: E''.
+Proof.
+apply/setP/subset_eqP/andP; split; apply/subsetP; rewrite/sub_mem => y.
+- move/imsetP => [] x.
+  rewrite in_setU.
+  move/orP => [] H0 H1; rewrite in_set.
+  - apply/orP; left.
+    by apply/imsetP; exists x.
+  - apply/orP; right.
+    by apply/imsetP; exists x.
+- rewrite in_setU.
+  move/orP => [].
+  - move/imsetP => [] x H0 H1.
+    apply/imsetP.
+    exists x; last by [].
+    rewrite in_setU.
+    by rewrite H0.
+  - move/imsetP => [] x H0 H1.
+    apply/imsetP.
+    exists x; last by [].
+    rewrite in_setU.
+    rewrite H0.
+    exact: orbT.
+Qed.
+
+Proposition image_inter :
+  f @: (E' :&: E'') \subset f @: E' :&: f @: E''.
+Proof.
+apply/subsetP; rewrite/sub_mem => y.
+move/imsetP => [] x H0 H1.
+rewrite in_setI in H0; move/andP in H0.
+move: H0 => [] H00 H01.
+rewrite in_setI.
+by apply/andP; split; apply/imsetP; exists x.
+Qed.
+
+Proposition image_réciproque_vide :
+  f @^-1: set0 = set0.
+Proof.
+apply/eqP.
+rewrite -subset0.
+apply/subsetP; rewrite/sub_mem => x.
+rewrite/preimset. (** definition of preimset *)
+rewrite in_set in_set0.
+by [].
+Qed.
+
+End Définitions_et_premiers_exemples.
