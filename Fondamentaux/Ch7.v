86abe890402dbd4c21fc5734569cb395209099ee
Zhan JING <147552860+zhanquen@users.noreply.github.com>
2025-06-28 18:11:22 +0800

    Update Ch7.v

--- Fondamentaux/Ch7.v
+++ Fondamentaux/Ch7.v
@@ -4,144 +4,309 @@ Set Implicit Arguments.
 Unset Strict Implicit.
 Unset Printing Implicit Defensive. 
 
-(**
-In this chapter, some properties and examples related to Relation are 
-demonstrated except those concerning a concrete number set.
-*)
+Section Définitions_et_premiers_exemples.
 
-Section Relation_d'équivalence.
+Variables (E F G: finType) (H : Type).
 
-Variable E : finType.
+Variables (f : {ffun E → F}) (g : {ffun F → G}) (h : {ffun G → H}).
 
-Variable (rE : rel E).
+Proposition fonc_comp_assoc : 
+  h \o (g \o f) =1 (h \o g) \o f.
+Proof.
+rewrite/eqfun => x.
+rewrite /=. (* trivial by calculation *)
+by [].
+Qed.
 
-Variables (trE : transitive rE) (rrE : reflexive rE) (srE : symmetric rE).
+Proposition Neutralité_de_l'identité_gauche : 
+  (@ id F) \o f =1 f.
+Proof.
+rewrite/eqfun => x.
+rewrite /=.
+by [].
+Qed.
 
-Variable rE_equivalence : transitive rE /\ reflexive rE /\ symmetric rE.
+Proposition Neutralité_de_l'identité_droite : 
+   f \o (@ id E) =1 f.
+Proof.
+rewrite/eqfun => x.
+rewrite /=.
+by [].
+Qed.
 
-Lemma equiv_E_repres (x y : E) : 
-  [set z | rE x z] = [set z | rE z y] <-> rE x y.
+(* p100 typo : h \o (g \o h) *)
+
+Proposition image_d'une_composée :
+  (g \o f) @: E \subset g @: F.
+Proof.
+apply/subsetP; rewrite/sub_mem => z.
+move/imsetP. (** definition of imset *)
+move => [] x H0.
+rewrite /= => H1.
+apply/imsetP.
+exists (f x); last by [].
+by [].
+Qed.
+
+Proposition image_vide : 
+  f @: set0 = set0.
+Proof.
+apply/eqP.
+rewrite -subset0.
+apply/subsetP; rewrite/sub_mem => y.
+move/imsetP.
+move=> [] x.
+rewrite in_set.
+by [].
+Qed.
+
+Variables (E' E'' : {set E}).
+
+Hypothesis (E'E : E' \subset E) (E''E : E'' \subset E).
+
+Proposition image_partie :
+  E' \subset E'' -> f @: E' \subset f @: E''.
+Proof.
+move=> H0.
+apply/subsetP; rewrite/sub_mem => y.
+move/imsetP => [] x H1 H2.
+apply/imsetP.
+move/subsetP in H0.
+rewrite/sub_mem in H0.
+exists x; last by [].
+apply: H0.
+by [].
+Qed.
+
+Proposition image_union :
+  f @: (E' :|: E'') = f @: E' :|: f @: E''.
+Proof.
+apply/setP/subset_eqP/andP; split; apply/subsetP; rewrite/sub_mem => y.
+- move/imsetP => [] x.
+  rewrite in_setU.
+  move/orP => [] H0 H1; rewrite in_set.
+  - apply/orP; left.
+    by apply/imsetP; exists x.
+  - apply/orP; right.
+    by apply/imsetP; exists x.
+- rewrite in_setU.
+  move/orP => [].
+  - move/imsetP => [] x H0 H1.
+    apply/imsetP.
+    exists x; last by [].
+    rewrite in_setU.
+    by rewrite H0.
+  - move/imsetP => [] x H0 H1.
+    apply/imsetP.
+    exists x; last by [].
+    rewrite in_setU.
+    rewrite H0.
+    exact: orbT.
+Qed.
+
+Proposition image_inter :
+  f @: (E' :&: E'') \subset f @: E' :&: f @: E''.
+Proof.
+apply/subsetP; rewrite/sub_mem => y.
+move/imsetP => [] x H0 H1.
+rewrite in_setI in H0; move/andP in H0.
+move: H0 => [] H00 H01.
+rewrite in_setI.
+by apply/andP; split; apply/imsetP; exists x.
+Qed.
+
+Variables F' F'' : {set F}.
+
+Proposition image_réciproque_vide :
+  f @^-1: set0 = set0.
+Proof.
+apply/eqP.
+rewrite -subset0.
+apply/subsetP; rewrite/sub_mem => x.
+rewrite/preimset. (** definition of preimset *)
+rewrite in_set in_set0.
+by [].
+Qed.
+
+Proposition image_réciproque_Image :
+  f @^-1: (f @: [set: E]) = [set: E].
+Proof.
+apply/setP/subset_eqP/andP; split.
+- rewrite/preimset.
+  apply/subsetP; rewrite/sub_mem => x; rewrite in_set.
+  move => H0.
+  by rewrite in_setT.
+- apply/subsetP; rewrite/sub_mem => x H0.
+  rewrite in_set.
+  apply/imsetP.
+  exists x; first by [].
+  by [].
+Qed.
+
+Proposition subset_stable_réciproque :
+  F' \subset F'' -> f @^-1: F' \subset f @^-1: F''.
+Proof.
+move => H0.
+apply/subsetP; rewrite/sub_mem => x.
+rewrite/preimset !in_set => H1.
+move/subsetP in H0.
+rewrite/sub_mem in H0.
+exact: H0.
+Qed.
+
+Proposition réciproque_union :
+  f @^-1: (F' :|: F'') = f @^-1: F' :|: f @^-1: F''.
+Proof.
+apply/setP/subset_eqP/andP; split; apply/subsetP; rewrite/sub_mem => x.
+- rewrite/preimset.
+  move=> H0.
+  rewrite in_set in_setU in H0.
+  rewrite in_setU.
+  rewrite !in_set.
+  by [].
+- by rewrite/preimset !in_set.
+Qed.
+
+Proposition réciproque_inter :
+  f @^-1: (F' :&: F'') = f @^-1: F' :&: f @^-1: F''.
+Proof.
+apply/setP/subset_eqP/andP; split; apply/subsetP; rewrite/sub_mem => x.
+- rewrite/preimset.
+  rewrite in_set.
+  move=> H0.
+  rewrite in_setI in H0.
+  rewrite in_setI.
+  rewrite !in_set.
+  by [].
+- by rewrite/preimset !in_set.
+Qed.
+
+End Définitions_et_premiers_exemples.
+
+Section Injectivité_Surjectivité.
+
+Variables (E F G : finType) (f : E -> F) (g : F -> G).
+
+Proposition comp_inj1 :
+  (injective f) /\ (injective g) -> injective (g \o f).
+Proof.
+case => H0 H1.
+move => x x'.
+rewrite /= => H2.
+apply: H0.
+apply: H1.
+by [].
+Qed.
+
+Proposition comp_inj2 :
+  injective (g \o f) -> injective f.
+Proof.
+move => H0 x x' H1.
+have H2 : g (f x) = g (f x') by rewrite H1.
+apply: H0.
+rewrite /=.
+by [].
+Qed.
+
+Definition surjective (E' F' : finType) (f' : E' -> F') := 
+  forall y : F', exists x : E', y = f' x.
+
+Proposition imfF_surj_E :
+  surjective f <-> (f @: E == [set: F]).
 Proof.
 split.
-- move/setP/subset_eqP/andP => []; move/subsetP; rewrite/sub_mem => H0 H1.
-  have H := H0 x.
-  rewrite !in_set in H.
-  move: (H (rrE x)).
-  by [].
 - move => H0.
-  apply/setP/subset_eqP/andP => []; split; apply/subsetP; rewrite/sub_mem => z'.
-  - rewrite in_set.
+  apply/eqP/setP/subset_eqP/andP; split.
+  - apply/subsetP; rewrite/sub_mem => y.
+    by rewrite in_set.
+  - apply/subsetP; rewrite/sub_mem => y.
     move => H1.
-    rewrite srE in H1.
-    rewrite in_set.
-    exact: (trE H1 H0).
-  - rewrite !in_set srE => H1.
-    exact: (trE H0 H1).
+    apply/imsetP.
+    move: (H0 y) => [x H2].
+    by exists x.  
+- move/eqP.
+  move => H0 y.
+  move/setP/subset_eqP/andP in H0.
+  move: H0 => [H1 H2].
+  move/subsetP in H2.
+  rewrite/sub_mem in H2.
+  have/imsetP H3 := H2 y (in_setT y).
+  move: H3 => [x H4 H5].
+  by exists x.
 Qed.
-
-Variable R : {set E}.
-
-(* Hypothesis H_R1 : forall x y, (x \in R) && (y \in R) -> x != y -> ~ (rE x y). *)
-
-Hypothesis H_R2 : forall z : E, exists! x : E, (x \in R) && (z \in [set z0 | rE x z0]).
-
-Proposition classe_equivalence_partition : 
-  [set: E] != set0 -> 
-  ([set: E] = \bigcup_ ( x in R ) [set z | rE x z]) /\ 
-  (forall x, x \in R -> [set z | rE x z] != set0) /\
-  (forall x y, x \in R /\ y \in R /\ x != y -> [disjoint [set z | rE x z] & [set z | rE y z]])
-  .
-move=> H0.
-split; last first.
-- split.
-  - move=> x' H1.
-    apply/set0Pn.
-    exists x'.
-    rewrite in_set.
-    exact: rrE.
-  - move=> x' y' [H1 [H2 H6]].
-    rewrite -setI_eq0.
-    apply: negbNE.
-    apply/set0Pn.
-    move => [z]; rewrite in_setI !in_set.
-    move/andP => [H3].
-    rewrite srE => H4.
-    have H5 := trE H3 H4.
-    apply equiv_E_repres in H5.
-    have H7 := H_R2 z.
-    rewrite/unique in H7.
-    move: H7 => [x'' [H7 H8]].
-    have Hx' := H8 x'; rewrite H1 in_set //= in Hx'.
-    have Hx'0 := Hx' H3.
-    have Hy' := H8 y'; rewrite H2 in_set srE //= in Hy'.
-    have Hy'0 := Hy' H4.
-    rewrite -Hx'0 -Hy'0 in H6.
-    by move/negP: H6 => [].
-- apply/setP/subset_eqP/andP; split; apply/subsetP; rewrite/sub_mem => x H1.
-  - apply/bigcupP.
-    have H2 := H_R2 x.
-    move: H2 => [x0].
-    rewrite/unique//=.
-    move => [/andP [H2 H3] H4].
-    exists x0.
-      by rewrite H2.
-    by [].
-    (* p264 mistake: please refer to this line *)  
-  - by rewrite in_setT.
-Qed.
-
-End Relation_d'équivalence.
-
-Section Relation_d'ordre.
-
-Variables E F : finType.
-
-Variables (rE : rel E) (rF : rel F).
-
-Variables (trF : transitive rF) (rrF : reflexive rF) (arF : antisymmetric rF).
-
-Variables (trE : transitive rE) (rrE : reflexive rE) (arE : antisymmetric rE).
-
-Variables rEor : transitive rE /\ reflexive rE /\ antisymmetric rE.
-
-Variables rFor : transitive rF /\ reflexive rF /\ antisymmetric rF.
-
-Let rEstor := [rel x y | (x != y) && rE x y].
-
-Proposition antisym_Estor : antisymmetric rEstor.
+  
+Proposition comp_surj1 : 
+  (surjective f) /\ (surjective g) -> surjective (g \o f).
 Proof.
-move => x y /=.
-rewrite andbA.
-move/andP => [H0 H1]; move: H0.
-move/andP => [H0 H2]; move: H0.
-move/andP => [H0 H3].
-apply: arE.
-by rewrite H1 H3.
+move => [H0 H1] z.
+have H2 := (H1 z); move: H2 => [y H2].
+have H3 := (H0 y); move: H3 => [x H3].
+exists x.
+rewrite /= -H3.
+by [].
+Qed.
+(* p118 Prop 3.8 typo: y = f(x) *)
+
+Proposition comp_surj2 :
+  surjective (g \o f) -> surjective g.
+Proof.
+move=> H0 z.
+have/= H1 := H0 z.
+move: H1 => [x H1].
+exists (f x).
+by [].
 Qed.
 
-Proposition tr_Estor : transitive rEstor.
+
+End Injectivité_Surjectivité.
+
+
+Section Bijectivité.
+
+Variables (E F : finType) (f : E -> F).
+
+Definition my_bijective := injective f /\ surjective f.
+
+(**
+??? : a problem of cop raises here: we are not allowed to define {ffun ...}
+when we encounter the composition.
+*)
+Proposition carac_bij31 :
+  (exists g , (g \o f =1 @id E) /\ (f \o g =1 @id F)) -> my_bijective.
 Proof.
-move => x y z /=.
-move/andP => [H0 Hyz].
-move/andP => [H1 Hxz].
-apply/andP; split; last first.
-- exact: (trE Hyz).
-- apply/negP.
-  move/eqP => eq_yz.
-  rewrite eq_yz in H0.
-  rewrite eq_yz in Hyz.
-  have eq_xz : x == z.
-    apply/eqP.
-    apply: antisym_Estor.
-    rewrite/rEstor /= andbA.
-    by rewrite H1 Hxz H0 Hyz.
-  move/negP in H1.
-  by [].
+move=> [g [Hgf Hfg]].
+rewrite/my_bijective; split.
+- apply: (@comp_inj2 _ _ _ _ g).
+  rewrite/injective=> x x'; rewrite/eqfun in Hgf.
+  by rewrite !Hgf.
+- Check comp_surj2.
+  apply: (@comp_surj2 _ _ _ g f).
+  rewrite/surjective.
+  rewrite/eqfun in Hfg.
+  move=> y; exists y.
+  by rewrite !Hfg.
+Qed.
+
+Proposition carac_bij12 : 
+  my_bijective -> forall y : F, exists! x : E, y = f x.
+Proof.
+move=> [Hi Hs] y.
+move: (Hs y) => [x H0].
+exists x.
+rewrite/unique; split; rewrite //.
+rewrite/injective in Hi.
+move => x' Hx'.
+move: (Hi x x').
+rewrite -Hx' H0 => H1.
+exact: H1.
 Qed.
 
 (**
-p270 Remarque 7.1 does not provide a clear reason for antisymetrique;
-and antireflexive could be demonstrated with antisymetrique
+??? : How can we create a function with help of the existence predicate?
 *)
-  
-End Relation_d'ordre.
+
+Proposition carac_bij23 : 
+  (forall y : F, exists! x : E, y = f x) -> exists g0 , (g0 \o f =1 @id E) /\ (f \o g0 =1 @id F).
+Admitted.
+
+End Bijectivité.
