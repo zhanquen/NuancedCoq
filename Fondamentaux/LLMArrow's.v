4e22d030ea0a2a256561ea92c0c78e3763896ea1
Zhan JING <147552860+zhanquen@users.noreply.github.com>
2026-03-05 02:28:20 +0800

    Implement proofs and add verification details
    Added verification details for Coq and MathComp versions. Implemented proofs for DictatorshipToAlt and UnaniousToAlt lemmas.

--- Fondamentaux/LLMArrow's.v
+++ Fondamentaux/LLMArrow's.v
@@ -1,6 +1,11 @@
 From Coq Require Import Init.Prelude Unicode.Utf8 Logic.FunctionalExtensionality.
 From mathcomp Require Import all_ssreflect.
 
+(* Verified in nix-shell:
+   - Coq: 8.17.1
+   - MathComp (ssreflect): 1.17.0
+*)
+
 Set Implicit Arguments.
 Unset Strict Implicit.
 Unset Printing Implicit Defensive.
@@ -57,13 +62,29 @@ Definition dictatorial_alt F := exists i : I, forall tup_r : L^n, forall a b : A
 
   Lemma DictatorshipToAlt : dictatorial F -> dictatorial_alt F.
   Proof.
-  Admitted.
+  move=> [j Hj]; exists j => tup_r a b.
+  by rewrite Hj.
+  Qed.
 
 Definition Unanimous_alt F := forall tup_r : L^n, forall a b : A, 
   (forall i, rl (tnth tup_r i) a b) -> (rl (F tup_r) a b).
 
   Lemma UnaniousToAlt : Unanimous F /\ IIA F -> Unanimous_alt F.
   Proof. 
-  Admitted.
+  move=> [Hun Hii] tup_r a b Hall.
+  pose r0 := tnth tup_r ord0.
+  pose const_r0 : L^n := [tuple r0 | i < n].
+  have Hpair : forall i, (rl (tnth tup_r i) a b) <-> (rl (tnth const_r0 i) a b).
+    move=> i.
+    rewrite /const_r0 tnth_mktuple.
+    split.
+    - move=> _; rewrite /r0; exact: Hall ord0.
+    - move=> _; exact: Hall i.
+  have Hii_ab := Hii a b tup_r const_r0 Hpair.
+  have HFconst : rl (F const_r0) a b.
+    rewrite /const_r0 Hun /r0.
+    exact: Hall ord0.
+  by move: Hii_ab => [_ H]; exact: H HFconst.
+  Qed.
 
 End Arrow.
